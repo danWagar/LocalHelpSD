404b42ad4cb7263f7b79e9fd3bc6cd8b0667585d
danWagar <dandwagar@gmail.com>
2020-05-18 13:10:38 -0700

    proper functionality for displaying date and time in messageHistory

--- src/Components/MessageHistory/MessageHistory.tsx
+++ src/Components/MessageHistory/MessageHistory.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useGetMessageHistoryQuery } from '../../generated/graphql';
 import './MessageHistory.css';
 
@@ -7,8 +7,14 @@ interface iMessageHistory {
   sender_id: number;
 }
 
+type dateTimeType = {
+  monthDay: string;
+  time: string;
+};
+
 const MessageHistory: React.FC<iMessageHistory> = (props) => {
   const { sender_id, receiver_id } = props;
+  //const { lastMessageDateTime, setLastMessageDateTime } = useState<dateTimeType>({monthDay: '', time: ''});
 
   const { data, loading, error } = useGetMessageHistoryQuery({
     variables: {
@@ -17,35 +23,46 @@ const MessageHistory: React.FC<iMessageHistory> = (props) => {
     },
   });
 
-  const TStoDisplayDate = (tsStr: string) => {
-    console.log(tsStr);
-    const ts = parseInt(tsStr);
-
+  const TStoDisplayDate = (ts: number) => {
     const date = new Date();
     date.setTime(ts);
-    date.setMinutes(date.getMinutes());
-    console.log(date.toString());
     const month = date.toLocaleString('default', { month: 'long' });
     const day = date.toJSON().slice(8, 10);
     let time = date.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });
     if (time.charAt(0) === '0') time = time.slice(1);
+    return { monthDay: month + ' ' + day, time: time };
+  };
 
-    console.log(month + ' ' + day);
-    console.log(time);
-
-    return { day: month + ' ' + day, time: time };
+  const getDateTimeJSX = (dateTime: dateTimeType, lastMsgDate: string, timeDifference: number) => {
+    if (dateTime.monthDay !== lastMsgDate) {
+      return (
+        <>
+          <div className="MessageHistory_date">{dateTime.monthDay}</div>
+          <div className="MessageHistory_time">{dateTime.time}</div>
+        </>
+      );
+    } else if (timeDifference > 600000) {
+      return <div className="MessageHistory_time">{dateTime.time}</div>;
+    }
   };
 
   const parseMsgHistory = () => {
+    let lastMsgDate = '';
+    let lastMsgMilliseconds = 0;
     return data?.getMessageHistory?.map((msg) => {
-      const dateTime = TStoDisplayDate(msg?.date_sent!);
-      return (
-        <p className="MessageHistory_message">
-          <span>{dateTime.day + ' ' + dateTime.time}</span>
+      const milliseconds = parseInt(msg?.date_sent!);
+      const dateTime = TStoDisplayDate(milliseconds);
+      const timeDifference = milliseconds - lastMsgMilliseconds;
+      const jsx = (
+        <div className="MessageHistory_message">
+          {getDateTimeJSX(dateTime, lastMsgDate, timeDifference)}
           <span>{msg?.subject}</span>
           <span>{msg?.body}</span>
-        </p>
+        </div>
       );
+      lastMsgDate = dateTime.monthDay;
+      lastMsgMilliseconds = milliseconds;
+      return jsx;
     });
   };
 
